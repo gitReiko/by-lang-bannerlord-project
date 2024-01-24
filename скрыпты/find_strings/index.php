81cc36ef84c9aa1759e6e6380be66ac86b32941a
Denis <denis.makouski@bseu.by>
2024-01-24 11:34:56 +0300

    ++

--- скрыпты/find_strings/index.php
+++ скрыпты/find_strings/index.php
@@ -1,19 +1,22 @@
 <?php 
 
-require_once 'classes\folder_files_getter.php';
-require_once 'classes\en_translation_adder.php';
-require_once 'classes\by_translation_adder.php';
-require_once 'classes\files_writer.php';
-
 // Error output
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Files include
+require_once __DIR__.'/classes/folder_files_getter.php';
+require_once __DIR__.'/classes/en_translation_adder.php';
+require_once __DIR__.'/classes/by_translation_adder.php';
+require_once __DIR__.'/classes/files_writer.php';
+
+// Consts define
 define('FIND_BY_ID', 'find_by_id');
 define('FIND_BY_TEXT', 'find_by_text');
 define('FIND_METHOD', FIND_BY_TEXT);
 
+// Code flow
 $TRANSLATIONS = array();
 
 $enAdder = new EnTranslationsAdder();
