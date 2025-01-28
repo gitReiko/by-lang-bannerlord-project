66f52eda091693dc95c66baa5920ee2b82ef8efe
Denis Makouski (Reiko) <khornau@gmail.com>
2025-01-28 16:32:18 +0300

    ++

--- скрыпты/cs_xml_to_xlst/obj/Debug/net9.0/cs_xml_to_xlst.AssemblyInfo.cs
+++ скрыпты/cs_xml_to_xlst/obj/Debug/net9.0/cs_xml_to_xlst.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cs_xml_to_xlst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f2fa121535f93e775a7e45340538d8e217e21a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462ae71d9b395590e5a3d090474e05267c96e346")]
 [assembly: System.Reflection.AssemblyProductAttribute("cs_xml_to_xlst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cs_xml_to_xlst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
